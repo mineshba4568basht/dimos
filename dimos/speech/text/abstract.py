47243a2a1d110a72020e5b90cc657a392eeebd70
lesh <lesh@sysphere.org>
2025-11-16 10:15:51 +0000

    speech initial sketch
    Former-commit-id: b33345058e8012fa267721300c4b25c90d8cbed6 [formerly 9af0f040d94ba8c05610daddc614d96e8e04d9fb] [formerly 15f09df8406b3ad8039f33afcdd09c87bdadcd83 [formerly afec45477b22f6971cde2ee340661350c06016c4]]
    Former-commit-id: 85456fea801c49ad69ccdb0096559c3e87dbeae3
    Former-commit-id: c5d5bd8028674e5fc92ae09c8b70aa09529a2ef2

---
+++ dimos/speech/text/abstract.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from reactivex import Observable
+from typing import Any
+
+
+class AbstractTextEmitter(ABC):
+    """Base class for components that emit audio."""
+
+    @abstractmethod
+    def emit_text(self) -> Observable:
+        """Create an observable that emits audio frames.
+
+        Returns:
+            Observable emitting audio frames
+        """
+        pass
+
+
+class AbstractTextConsumer(ABC):
+    """Base class for components that consume audio."""
+
+    @abstractmethod
+    def consume_text(self, text_observable: Observable) -> 'AbstractTextConsumer':
+        """Set the audio observable to consume.
+
+        Args:
+            audio_observable: Observable emitting audio frames
+
+        Returns:
+            Self for method chaining
+        """
+        pass
+
+
+class AbstractTextTransform(AbstractTextConsumer, AbstractTextEmitter):
+    """Base class for components that both consume and emit audio.
+
+    This represents a transform in an audio processing pipeline.
+    """
+    pass
+
+
+# check input query stream for agents
+# normalizer comments
+# check input query stream for agents
+# check simpleagent, readme
