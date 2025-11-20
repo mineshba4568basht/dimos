7a7e4627ff5e37180b28fa457dd60dc166a12ecc
lesh <lesh@sysphere.org>
2025-11-20 08:30:41 +0000

    making the motd nicer
    Former-commit-id: c0ff204dc93ea4dbccd3aa797f786fed502ec7c4 [formerly 0298de4ab37385302e5e6b7a8a96d8bfdc4ccfbd] [formerly 3215faf5c014759fd0c5658509386e6a4d260912 [formerly a2d2b9f2d6e161970a74e32ee69e5a34741f5ee0]]
    Former-commit-id: d9d60d5f7868c1929dc01ab015ddd5fb4d5fa53a
    Former-commit-id: ac3e600604609396f96286aa9705338540f8e159

--- builddev.sh
+++ builddev.sh
@@ -1,1 +1,5 @@
-docker rm -f dimos-dev; docker build -t dimensionalos/dev-base docker/dev/base/
+docker rm -f dimos-dev
+docker build \
+    --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) \
+    --build-arg GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+    -t dimensionalos/dev-base docker/dev/base/
