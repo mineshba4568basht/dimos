3f13abf49e275724787c476defbb263d050632b3
Ivan Nikolic <lesh@sysphere.org>
2026-03-14 19:54:23 +0800

    delete livechannel/ backwards-compat shim

--- dimos/memory2/livechannel/__init__.py
+++
@@ -1,6 +0,0 @@
-# Backwards-compat shim: livechannel/ was renamed to notifier/.
-# Kept so that deserialize_component() can resolve old registry entries.
-from dimos.memory2.notifier.base import Notifier
-from dimos.memory2.notifier.subject import SubjectNotifier
-
-__all__ = ["Notifier", "SubjectNotifier"]
